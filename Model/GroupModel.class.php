2f02f3ddcd1cf546112c6efd891c8109aaa0e327
gscsdlz <lz842063523@foxmail.com>
2016-10-25 16:39:20 +0800

    update this file

--- Model/GroupModel.class.php
+++ Model/GroupModel.class.php
@@ -1,15 +1,12 @@
 <?php
 //用户组相关类
-class Group {
+class Group extends BaseModel{
 	private $groupName = '';
 	private $link;
 	
 	function __construct() {
-		global $config;
-		$this->link = new mysql ( $config );
-		if (isset ( $_POST ['groupName'] ))
-			$this->groupName = $this->link->real_escape_string ( $_POST ['groupName'] );
-		$this->username = $_SESSION ['username'];
+		parent::__construct("group");
 	}
+
 }
 ?>
